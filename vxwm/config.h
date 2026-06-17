a5e56ab8e1f3173f70de0c9f6e1e829878868a71
kurokilab <kuroki@tutanota.de>
2026-06-17 20:59:02 +0300

    fix: fonts in vxwm/config.h

--- vxwm/config.h
+++ vxwm/config.h
@@ -7,8 +7,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=10" };
+static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=10";
 #define COORDINATES_STYLE "[%d|%d]" /* The style of coordinates displayed in bar, do not remove %d. */
 
 static MAYBE_CONST char normbgcolor[]           = "#282828"; /* gruvbox bg0 */
